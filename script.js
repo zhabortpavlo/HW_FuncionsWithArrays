c1758f3cd14296d0861afb189f01f404f02cf498
Pavlo Zhabort <zhabortp@gmail.com>
2024-03-14 11:20:37 +0200

    removed uselless code and formated document

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const array = [1,2,3,4,5,6,7];
+const array = [1, 2, 3, 4, 5, 6, 7];
 
-const removeElement = function(array,item) {
-const elemToRemove = item;
-const filteredArr =  array.filter(function(elem) {
-return elem !== elemToRemove;
-})
-console.log(filteredArr)
-}
+const removeElement = function (array, item) {
+  const filteredArr = array.filter(function (elem) {
+    return elem !== item;
+  });
+  console.log(filteredArr);
+};
 
-removeElement(array,6);
+removeElement(array, 6);
